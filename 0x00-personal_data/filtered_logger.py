d8e56b732bf86bc96702d3054d7c9613487125ef
Yordi-Dereje <yordadereje@gmail.com>
2023-02-02 21:53:07 +0300

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import typing
 
 
 def filter_datum(fields: typing.List[str], redaction: str, message: str,
-                separator: str) -> str:
+                 separator: str) -> str:
     """ method to filter data """
     for f in fields:
         message = re.sub('{}=.+?{}'.format(f, separator),
